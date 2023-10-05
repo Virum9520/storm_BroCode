9506543a5ecc23dd2463e29e8d80cf62321d4106
Virum9520 <virumranka5503@gmail.com>
2023-10-05 11:21:41 +0530

    Delete users directory

--- users/functions.py
+++
@@ -1,5 +0,0 @@
-def handle_uploaded_file(f):  
-    with open('users/static/users/images/'+f.name, 'wb+') as destination:  
-        for chunk in f.chunks():  
-            destination.write(chunk)
-    return chunk
